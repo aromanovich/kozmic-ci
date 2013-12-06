3facbb667738044c12c52571b5166591040adf3e
Anton Romanovich <anthony.romanovich@gmail.com>
2013-12-06 20:33:53 +0600

    Make test.sh use kozmic.config_local.TestingConfig by default

--- test.sh
+++ test.sh
@@ -1,1 +1,11 @@
-PYTHONPATH=.:$PYTHONPATH KOZMIC_CONFIG=kozmic.config.TestingConfig py.test $@
+#!/bin/bash
+if [[ -z $KOZMIC_CONFIG ]]; then
+    if [[ ! -f ./kozmic/config_local.py ]]; then
+        echo -e "./kozmic/config_local.py does not exist."\
+                "\nPlease create it using ./kozmic/config_local.py-dist as an example."
+        exit 1
+    fi
+    KOZMIC_CONFIG=kozmic.config_local.TestingConfig
+fi
+
+PYTHONPATH=.:$PYTHONPATH KOZMIC_CONFIG=$KOZMIC_CONFIG py.test $@
