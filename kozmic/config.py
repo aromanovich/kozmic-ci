087478251d58007c152b9e5af215eca214d8d426
Anton Romanovich <anthony.romanovich@gmail.com>
2013-12-06 20:33:16 +0600

    Remove all host-specific settings from kozmic.config

--- kozmic/config.py
+++ kozmic/config.py
@@ -8,9 +8,6 @@ Kozmic default configurations
 import os
 
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
 class DefaultConfig(object):
     DEBUG = False
     TESTING = False
@@ -19,6 +16,7 @@ class DefaultConfig(object):
     KOZMIC_REDIS_HOST = 'localhost'
     KOZMIC_REDIS_PORT = 6379
     KOZMIC_REDIS_DATABASE = 0
+
     BROKER_URL = 'redis://{host}:{port}/{db}'.format(
         host=KOZMIC_REDIS_HOST,
         port=KOZMIC_REDIS_PORT,
@@ -34,32 +32,16 @@ class DefaultConfig(object):
 
 class DevelopmentConfig(DefaultConfig):
     DEBUG = True
-    SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-    KOZMIC_GITHUB_CLIENT_ID = '2ee444daf6b2437b8b48'
-    KOZMIC_GITHUB_CLIENT_SECRET = '5412fdcb332cf1cbcac4cbd28093792523d27a4f'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:@192.168.33.10/kozmic'
-    SERVER_NAME = '217.76.184.49:10001'
-    SESSION_COOKIE_DOMAIN = SERVER_NAME
-    TAILER_URL_TEMPLATE = 'ws://217.76.184.49:4506/{task_uuid}/'
+    SECRET_KEY = 'development'
 
 
 class TestingConfig(DefaultConfig):
     TESTING = True
     SECRET_KEY = 'testing'
+    SERVER_NAME = 'kozmic.test'
     KOZMIC_GITHUB_CLIENT_ID = ''
     KOZMIC_GITHUB_CLIENT_SECRET = ''
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:@192.168.33.10/kozmic_test'
-    # We need to specify LOGIN_DISABLED = False explicitly.
-    # Otherwise Flask-Login will turn off `login_required`
-    # decorator because of TESTING variable.
+    # We need to specify LOGIN_DISABLED = False explicitly,
+    # otherwise Flask-Login will turn off `login_required`
+    # decorator because of TESTING variable:
     LOGIN_DISABLED = False
-    SERVER_NAME = 'kozmic.test'
-    SESSION_COOKIE_DOMAIN = SERVER_NAME
-
-    KOZMIC_REDIS_HOST = '192.168.33.20'
-    KOZMIC_REDIS_PORT = 6379
-    KOZMIC_REDIS_DATABASE = 1
-    BROKER_URL = 'redis://{host}:{port}/{db}'.format(
-        host=KOZMIC_REDIS_HOST,
-        port=KOZMIC_REDIS_PORT,
-        db=KOZMIC_REDIS_DATABASE)
