3b7361aea593cb66012a20f2ba738252447b7882
Anton Romanovich <anthony.romanovich@gmail.com>
2014-01-16 21:13:32 +0600

    Show an error when manage.py being run without KOZMIC_CONFIG specified

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
+import os
+
 from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.migrate import MigrateCommand
 
 import kozmic
 
@@ -11,4 +13,7 @@ manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
+    if 'KOZMIC_CONFIG' not in os.environ:
+        print('Did you forget to set KOZMIC_CONFIG environment variable?')
+        exit(1)
     manager.run()
